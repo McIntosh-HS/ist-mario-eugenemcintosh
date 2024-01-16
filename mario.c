e7320c6e64b8292e6958731304d75cda3ba2584b
eugenemcintosh <2027cha.eugene@fcboe.org>
2024-01-16 14:30:11 -0500

    Completed mario assignment

--- mario.c
+++ mario.c
@@ -2,6 +2,14 @@
 
 int main()
 {
-	printf("Hello, IST!\n");
+	printf("	#\n");
+        printf("       ##\n");
+	printf("      ###\n");
+	printf("     ####\n");
+	printf("    #####\n");
+	printf("   ######\n");
+	printf("  #######\n");
+	printf(" ########\n");
+	printf("#########\n");
 	return 0;
 }
